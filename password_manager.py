4705a0f9cae6f31e2edb8b834b65ef91720872e7
Khavushka <khavushka@gmail.com>
2023-02-17 21:37:35 +0100

    Encrypting and decrypting password

--- password_manager.py
+++ password_manager.py
@@ -1,12 +1,28 @@
-pwd = input("What is the master password? ")
+from cryptography.fernet import Fernet
+
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+key = load_key()
+fer = Fernet(key)
+
+'''
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+'''
 
 def view():
     with open('passwords.txt', 'r') as f:
         for line in f.readlines():
             # rstrip() - removes any trailing characters
-            data = (line.rstrip())
-            user, passw = data.split(" | ")
-            print("User:", user, "| Password:", passw)
+            data = line.rstrip()
+            user, passw = data.split("|")
+            print("User:", user, "| Password:", fer.decrypt(passw.encode()).decode())
 
 def add():
     name = input("Account Name: ")
@@ -14,13 +30,13 @@ def add():
 
     # 'a' means append mode
     with open('passwords.txt', 'a') as f:
-        f.write(name + " | " + pwd + "\n")
+        f.write(name + " | " + fer.encrypt(pwd.encode()).decode() + "\n")
 
 while True:
     mode = input("Would you like to add a new password or view existing ones (view, add), press q to quit? ").lower()
     if mode =="q":
         break
-    elif mode == "view":
+    if mode == "view":
         view()
     elif mode == "add":
         add()
